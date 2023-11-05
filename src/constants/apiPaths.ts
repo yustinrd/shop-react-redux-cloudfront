15797f62c0a82bfe08fedab67d8487a53f62b3cc
Roman_Iustin <roman_iustin@epam.com>
2023-11-05 19:06:07 +0300

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://143pa0qv4m.execute-api.us-west-1.amazonaws.com/dev",
   bff: "https://4yokdcakqb.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
